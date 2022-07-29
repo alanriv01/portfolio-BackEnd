4b57d2d8523253d0b5aa126a3e62f633490706ed
alan rivadeneira <alanrivadeneira1991@gmail.com>
2022-07-29 03:23:42 -0300

    actualizado

--- backend/src/main/java/com/portfolio/rivadeneiraalan/Controller/PersonaController.java
+++ backend/src/main/java/com/portfolio/rivadeneiraalan/Controller/PersonaController.java
@@ -5,6 +5,7 @@ import com.portfolio.rivadeneiraalan.Entity.Persona;
 import com.portfolio.rivadeneiraalan.Interface.IpersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,18 +26,21 @@ public class PersonaController {
         return ipersonaService.getPersona();
     }
     
+    @PreAuthorize("HasRole('ADMIN')")
     @PostMapping("/personas/crear")
     public String createPersona(@RequestBody Persona persona){
         ipersonaService.savePersona(persona);
         return "La persona fue creada con exito";
     }
     
+    @PreAuthorize("HasRole('ADMIN')")
     @DeleteMapping("/personas/borrar/{id}")
     public String deletePersona(@PathVariable Long id){
         ipersonaService.deletePersona(id);
         return "La persona fue eliminada con exito";
     }
     
+    @PreAuthorize("HasRole('ADMIN')")
     @PutMapping("/persona/editar/{id}")
     public Persona editPersona(@PathVariable Long id,
                                @RequestParam("nombre") String nuevoNombre,
---
+++ backend/src/main/java/com/portfolio/rivadeneiraalan/Security/Controller/Mensaje.java
@@ -0,0 +1,27 @@
+
+package com.portfolio.rivadeneiraalan.Security.Controller;
+
+
+public class Mensaje {
+    private String mensaje;
+    
+    //constructores
+
+    public Mensaje() {
+    }
+
+    public Mensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+    
+    //getter and setters
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+    
+}
