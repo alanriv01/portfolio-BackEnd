5e1d527316e9b0c2901a72624798d26cc9619251
alan rivadeneira <alanrivadeneira1991@gmail.com>
2022-07-30 12:06:21 -0300

    modificaciones

--- src/main/java/com/portfolio/rivadeneiraalan/Controller/CAcercade.java
+++ src/main/java/com/portfolio/rivadeneiraalan/Controller/CAcercade.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/acercade")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://portfiolio-argprogr.web.app/")
 public class CAcercade {
     @Autowired
     SAcercade sAcercade;
